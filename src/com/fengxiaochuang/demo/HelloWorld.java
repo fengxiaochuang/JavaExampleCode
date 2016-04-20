65a1eaa69eb5771c3e5aa595ba7e6171051493e5
lanxinxichen <lanxinxichen@126.com>
2016-04-20 08:59:58 +0800

    使用idea进行第一次提交

---
+++ src/com/fengxiaochuang/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.fengxiaochuang.demo;
+
+/**
+ * Created by Administrator on 2016/4/19.
+ *
+ * @author lanxixnichen@126.com
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
